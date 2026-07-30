b81d37a790197e57db57ff5f7da622684f48d7ba
Hidayath <idayatholaogou@gmail.com>
2026-07-30 21:22:49 +0100

    Suppression du fichier test_connexion

--- test_connexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/config/Database.php';
-
-try {
-    $pdo = Database::getInstance();
-    echo "Connexion reussie a la base PostgreSQL !";
-} catch (Exception $e) {
-    echo "Erreur : " . $e->getMessage();
-}
